8606232fad03c1883255544cb0d03c681698456e
skihyeon <hero981001@gmail.com>
2023-10-25 21:58:41 +0900

    Modifing Script

--- script.py
+++ script.py
@@ -1,32 +1,21 @@
-import subprocess
 import argparse
-
-def run_train(args):
-    cmd = [
-        "python", "/mnt/data/train.py",
-        "--exp_name", args.exp_name,
-        # ... 다른 인자들 ...
-    ]
-    subprocess.run(cmd)
-
-def run_inference(args):
-    cmd = [
-        "python", "/mnt/data/inference.py",
-        "--checkpoint", args.checkpoint,
-        # ... 다른 인자들 ...
-    ]
-    subprocess.run(cmd)
+from train import train_main
+from inference import inference_main
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run train or inference scripts.")
     parser.add_argument("action", choices=["train", "inference"], help="Action to perform: train or inference.")
     parser.add_argument("--exp_name", type=str, help="Experiment name for training.")
     parser.add_argument("--checkpoint", type=str, help="Model checkpoint path for inference.")
-    # ... 여기에 다른 인자들을 추가 ...
 
     args = parser.parse_args()
 
+    if args.action == "train" and args.exp_name is None:
+        parser.error("--exp_name is required for training!")
+    elif args.action == "inference" and args.checkpoint is None:
+        parser.error("--checkpoint is required for inference!")
+
     if args.action == "train":
-        run_train(args)
+        train_main(args.exp_name) 
     elif args.action == "inference":
-        run_inference(args)
+        inference_main(args.checkpoint)
