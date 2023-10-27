5a89bc5a6f5682ecdd3f616ae477f0d03a4afd5e
skihyeon <hero981001@gmail.com>
2023-10-27 16:29:22 +0900

    Modify script

--- script.py
+++ script.py
@@ -1,21 +1,28 @@
 import argparse
-from train import train_main
-from inference import inference_main
+
+import inference
+import train
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run train or inference scripts.")
-    parser.add_argument("action", choices=["train", "inference"], help="Action to perform: train or inference.")
-    parser.add_argument("--exp_name", type=str, help="Experiment name for training.")
-    parser.add_argument("--checkpoint", type=str, help="Model checkpoint path for inference.")
+    """
+    Usage:
+        python3 script.py train --exp_name ...
+        python3 script.py inference --checkpoint ...
+    """
+    parser = argparse.ArgumentParser(
+        description="Run train or inference scripts."
+    )
+    parser.add_argument("action", choices=["train", "inference"])
+    parser.add_argument("--exp_name", type=str)
+    parser.add_argument("--checkpoint", type=str)
 
     args = parser.parse_args()
 
-    if args.action == "train" and args.exp_name is None:
-        parser.error("--exp_name is required for training!")
-    elif args.action == "inference" and args.checkpoint is None:
-        parser.error("--checkpoint is required for inference!")
-
     if args.action == "train":
-        train_main(args.exp_name) 
+        if args.exp_name is None:
+            parser.error("--exp_name is required for training!")
+        train.main(args.exp_name)
     elif args.action == "inference":
-        inference_main(args.checkpoint)
+        if args.checkpoint is None:
+            parser.error("--checkpoint is required for inference!")
+        inference.main(args.checkpoint)
