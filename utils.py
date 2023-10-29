7edb4fd6428bb510a1548ca6e8d4b0ee8007c4c0
skihyeon <hero981001@gmail.com>
2023-10-29 14:48:44 +0900

    Change path

--- utils.py
+++ utils.py
@@ -2,12 +2,10 @@ import os
 import glob
 import yaml
 
-def load_config(config_dir='configs'):
-    script_dir = os.path.dirname(__file__)
-    config_dir = os.path.join(script_dir, config_dir)
-    config_files = glob.glob(os.path.join(config_dir, '*_config.yaml'))
-    
+def load_config(config_dir='./configs'):
+    config_files = glob.glob('*.yaml')
     config = {}
+
     for config_file in config_files:
         if os.path.exists(config_file):
             with open(config_file, 'r') as f:
