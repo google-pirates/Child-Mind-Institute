ebcb635012445a55cd7b0b61a6bf9f83ce1133b2
skihyeon <hero981001@gmail.com>
2023-11-11 04:56:52 +0900

    Implement LPF

--- inference.py
+++ inference.py
@@ -5,38 +5,67 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
 
+from sklearn.metrics import mean_squared_error
+from scipy.signal import argrelmin, argrelmax
+from scipy import signal
+from scipy.signal import filtfilt, butter
+
 from data import ChildInstituteDataset, preprocess, to_list, extract_keys
 
-def inference(model, test_dataloader: DataLoader):
-    all_series_ids = []
-    all_steps = []
-    all_events = []
-    all_scores = []
-    all_dates = []
+def lpf(wave, fs=12*60*24, fe=60, n=3):
+    if len(wave) >= 6:
+        nyq = fs / 2.0
+        b, a = butter(1, fe/nyq, btype='low')
+        wave = wave.ravel()
+        for i in range(n):
+            wave = filtfilt(b, a, wave)
+    return wave
+
+def inference(model_path: str, test_dataloader: DataLoader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.jit.load(model_path, map_location=device)
+    model.to(device)
+    model.eval()
+
+    all_series_ids, all_steps, all_events, all_scores, all_dates = [], [], [], [], []
 
     with torch.no_grad():
         for batch in tqdm(test_dataloader, desc='Inference'):
-            # inputs = batch['X'].to(device)
-            series_id = batch['series_id'][0]
-            step = batch['step'][-1].item()
-            date = batch['date'][-1].item()
-            # print("X:", inputs.shape)
-            outputs = model({'X': batch['X'].permute(0,2,1)})
+            inputs, series_ids = batch['X'].to(device), batch['series_id']
+            steps, dates = batch['step'].cpu().numpy(), batch['date'].cpu().numpy()
+            outputs = model({'X': inputs}).view(-1)
 
-            probabilities = torch.sigmoid(outputs)
-            predictions = (probabilities > 0.5).float()
-            scores = probabilities.detach().cpu().numpy()
+            scores = torch.sigmoid(outputs).detach().cpu().numpy()
+            if scores.ndim > 0 and scores.size > 0:
+                before_RMSE = np.sqrt(mean_squared_error(scores, np.zeros_like(scores)))
+                filtered_scores = lpf(scores)  # lpf 함수를 호출하기 전에 길이 검사를 수행합니다.
+                after_RMSE = np.sqrt(mean_squared_error(filtered_scores, np.zeros_like(filtered_scores)))
+                decay_ratio = before_RMSE / after_RMSE if after_RMSE else 1
+                filtered_scores *= decay_ratio
+            else:
+                # scores가 적절한 형상이 아닐 경우 RMSE 계산을 건너뜁니다.
+                filtered_scores = scores
 
-            # events = ['wakeup' if pred == 1 else 'onset' for pred in predictions]
+            onset_candi = argrelmin(filtered_scores, order=12*60*6)[0]
+            wakeup_candi = argrelmax(filtered_scores, order=12*60*6)[0]
+            
+            for idx in onset_candi:
+                if filtered_scores[idx] < 0.5:
+                    all_series_ids.append(series_ids[idx])
+                    all_steps.append(steps[idx])
+                    all_events.append('onset')
+                    all_scores.append(filtered_scores[idx])
+                    all_dates.append(dates[idx])
 
-            all_series_ids.extend([series_id] * len(predictions))
-            all_steps.extend([step] * len(predictions))
-            # all_events.extend(events)
-            all_events.append(predictions)
-            all_scores.extend(["{:.5f}".format(score.item()) for score in scores])
-            all_dates.append(date)
+            for idx in wakeup_candi:
+                if filtered_scores[idx] >= 0.5:
+                    all_series_ids.append(series_ids[idx])
+                    all_steps.append(steps[idx])
+                    all_events.append('wakeup')
+                    all_scores.append(filtered_scores[idx])
+                    all_dates.append(dates[idx])
 
-    submission = pd.DataFrame({
+    return pd.DataFrame({
         'series_id': all_series_ids,
         'step': all_steps,
         'date': all_dates,
@@ -44,80 +73,32 @@ def inference(model, test_dataloader: DataLoader):
         'score': all_scores
     })
 
-    return submission
-
-
 def main(config):
-    # Load preprocessed data for inference
     test_data_path = config.get('general').get('test_data').get('path')
-    test_data = pd.read_parquet(test_data_path, 
-                                columns=['series_id', 'timestamp', 'step', 'anglez', 'enmo'])
-
-    test_data['event'] = 0
+    test_data = pd.read_parquet(test_data_path)
+    test_data['event'] = -1
     test_data = test_data[['series_id', 'timestamp', 'step', 'event', 'anglez', 'enmo']]
-    # test_data['timestamp'] = pd.to_datetime(test_data['timestamp'], format='%Y-%m-%d')
-    unique_series_ids = test_data['series_id'].unique()
+
+    window_size, step_size = config.get('inference').get('window_size'), config.get('inference').get('step')
     all_submissions = []
 
-    ## Load model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = torch.jit.load(config.get('general').get('checkpoint'), map_location=device)
-    model.to(device)
-    model.eval()
-
-    for series_id in unique_series_ids:
+    for series_id in test_data['series_id'].unique():
         series_data = test_data[test_data['series_id'] == series_id].copy()
         series_data.reset_index(drop=True, inplace=True)
-
         preprocessed_series_data = preprocess(series_data)
+        test_list, test_keys = to_list(preprocessed_series_data, window_size, config, step_size), extract_keys(preprocessed_series_data, window_size, step_size)
+        
+        for key, item in zip(test_keys, test_list):
+            key['X'] = item
 
-        window_size = config.get('inference').get('window_size')
-        step_size = config.get('inference').get('step')
-        test_list = to_list(preprocessed_series_data, window_size, config, step_size)
-        test_keys = extract_keys(preprocessed_series_data, window_size, step_size)
-
-        for i, key in enumerate(test_keys):
-            key['X'] = test_list[i]
-
-        test_dataset = ChildInstituteDataset(test_keys)
-        test_dataloader = DataLoader(test_dataset,
-                                    batch_size=config.get('inference').get('batch_size'),
-                                    shuffle=False
-                                    # num_workers=os.cpu_count()
-                                    )
-
-        submission = inference(model, test_dataloader=test_dataloader)
-        ## rolling
-        submission['event'] = submission['event'].rolling(window=7, min_periods=1).apply(
-            lambda x: x.mode()[0] if not x.mode().empty else np.nan, raw=False
-        )
-
-        ## droping
-        ## event 드랍 시에 onset 이벤트는 처음을 남기고, wakeup 이벤트는 마지막을 남기도록 수정
-        onset_df = submission[submission['event'] == 0.0].drop_duplicates(subset=['date'], keep='first')
-        wakeup_df = submission[submission['event'] == 1.0].drop_duplicates(subset=['date'], keep='last')
-        droped_submission = pd.concat([onset_df, wakeup_df]).sort_values(['date', 'event'])
-        droped_submission['event'] = droped_submission['event'].map({0.0: 'onset', 1.0: 'wakeup'})
-        all_submissions.append(droped_submission)
-        ##
-        # submission = submission.drop_duplicates(subset=['date', 'event'], keep='first')
-        # submission['event'] = submission['event'].map({0.0: 'onset', 1.0: 'wakeup'})
-        # all_submissions.append(submission)
-
+        submission = inference(config.get('general').get('checkpoint'), DataLoader(ChildInstituteDataset(test_keys), batch_size=config['inference']['batch_size'], shuffle=False, pin_memory=True))
+        all_submissions.append(submission)
 
     final_submission = pd.concat(all_submissions).reset_index(drop=True)
     final_submission['score'] = final_submission['score'].astype(float)
-
-    ## 이벤트가 onset 인 경우 스코어 반전
-    final_submission['score'] = np.where( final_submission['event'] == 'onset',
-                                         (final_submission['score']),
-                                         (1-final_submission['score']))
-    
-    final_submission['score'] = final_submission['score'].round(5)
     final_submission = final_submission.sort_values(['series_id', 'step']).reset_index(drop=True)
     final_submission['row_id'] = final_submission.index.astype(int)
     final_submission = final_submission[['row_id', 'series_id', 'step', 'event', 'score']]
-
-    final_submission.to_csv('submission2.csv', index=False)
+    final_submission.to_csv('submission.csv', index=False)
+    
     return final_submission
-
