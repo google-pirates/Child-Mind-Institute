e1b61e0de269b10423e5ab7bceab0aaa4c9b123e
skihyeon <hero981001@gmail.com>
2023-12-03 22:32:22 +0900

    Implement train

--- train.py
+++ train.py
@@ -19,32 +19,9 @@ from data import ChildInstituteDataset, preprocess, to_list, extract_keys
 from utils import make_logdir
 import models
 
-def train(config: dict, model: nn.Module, train_dataloader: DataLoader,
-          valid_dataloader: DataLoader, writer) -> nn.Module:
-    """
-    Train model, save and return best model.
-
-    Parameters:
-    - config (dict): Configuration dictionary.
-    - model (nn.Module): Model to train.
-    - train_dataloader (DataLoader): DataLoader for train data.
-    - valid_dataloader (DataLoader): DataLoader for validation data.
-
-    Returns:
-    - nn.Module: The model based on lowest valid loss.
-    """
+def train(config, model, train_dataloader, valid_dataloader):
     torch.manual_seed(config.get('train').get('random_seed'))
 
-    model_save_dir = os.path.join(writer.log_dir, 'saved_models') 
-    if not os.path.exists(model_save_dir):
-        os.makedirs(model_save_dir)
-    model_save_path = os.path.join(model_save_dir, 'best_model.pth')
-
-    optimizer_name = config.get('train').get('optimizer')
-    num_epochs = config.get('train').get('epochs')
-    lr = config.get('train').get('learning_rate')
-
-    ## Set Criterion
     criterion_config = config.get('train').get('criterion', {})
     criterion_name = criterion_config.get('name')
     criterion_params = criterion_config.get('params', {})
@@ -54,232 +31,177 @@ def train(config: dict, model: nn.Module, train_dataloader: DataLoader,
 
     criterion = criterion_class(**criterion_params)
 
-    ## Set Optimizer
-    optimizer_class = getattr(optim, optimizer_name, None)
+    optimizer_name = config.get('train').get('optimizer')
+    optimizer_class = getattr(torch.optim, optimizer_name, None)
     if optimizer_class is None:
         raise ValueError(f"Optimizer {optimizer_name} not found in torch.optim")
 
+    lr = config.get('train').get('learning_rate')
     optimizer = optimizer_class(model.parameters(), lr=lr)
-
-    ## Set scheduler
-    scheduler_name = config.get('train').get('scheduler', 'StepLR')
-    scheduler_params = config.get('train').get('scheduler_params', {'step_size': 10, 'gamma': 0.1})
-    scheduler_class = getattr(lr_scheduler, scheduler_name, None)
-    if scheduler_class is None:
-        raise ValueError(f"Scheduler {scheduler_name} not found in torch.optim.lr_scheduler")
-    scheduler = scheduler_class(optimizer, **scheduler_params)
-    if scheduler_params is None:
-        scheduler_params = {'step_size': 10, 'gamma': 0.1} ## need to refactoring
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    best_model = None
-    best_loss = float("inf")
-    patience_count = 0
-    patience = config.get('train').get('patience')
+    # Training loop
+    total_train_loss = 0.0
+    total_train_corrects = 0
+    total_train_samples = 0
+    
+    train_labels = []
+    train_preds = []
+    
+    model.train()
+    for batch in tqdm(train_dataloader, desc='Training'):
+        inputs = batch['X'].to(device)
+        labels = batch['y'].to(device).squeeze()
 
-    # for tensorboard logging
-    train_losses = []
-    valid_losses = []
-    train_total_predictions = []
-    train_total_labels = []
-    valid_total_predictions = []
-    valid_total_labels = []
+        optimizer.zero_grad()
+        outputs, hidden = model({'X':inputs, 'y':labels})
+        loss = criterion(outputs.reshape(-1, config.get('train').get('n_features')), labels.long().flatten())
+        loss.backward()
+        optimizer.step()
 
-    for epoch in tqdm(range(num_epochs), desc='epoch'):
-        # Training phase
-        model.train()
-        training_loss = 0.0
-        train_corrects = 0
-        train_total_samples = 0
-        for batch in tqdm(train_dataloader, desc='train iter'):
-            ## data에서 X, y 정의
-            inputs = batch['X']
-            labels = batch['y'].squeeze()
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs, hiddens = model({'X': inputs, 'y': labels}) # [batch_size, 1]
-
-            loss = criterion(outputs.reshape(-1, config.get('train').get('n_features')), labels.long().flatten())
-            loss.backward()
-            optimizer.step()
-            training_loss += loss.item()
-
-            ## accuracy 추가
-            predictions = outputs.argmax(axis=-1)
-            train_corrects += torch.sum(predictions == labels)
-            train_total_samples += inputs.size(0)
-
-            train_total_predictions.extend(predictions.cpu().tolist())
-            train_total_labels.extend(labels.tolist())
-
-        avg_train_loss = training_loss / len(train_dataloader)
-        train_losses.append(avg_train_loss)
-        train_accuracy = train_corrects.double() / train_total_samples
-
-        print(f"[Epoch {epoch + 1}/{num_epochs}] "
-              f"Training Loss: {avg_train_loss:.04f} | "
-              f"Training Accuracy: {train_accuracy:.04f} | ")
-        writer.add_scalar('Loss/train', avg_train_loss, epoch)
-        writer.add_scalar('Accuracy/train', train_accuracy, epoch)
-
-        train_results = np.array(
-            [re.sub(' {2,}', ' ', i.strip()).split()[1:-1]
-             for i
-             in classification_report(np.array(train_total_labels).reshape(-1), np.array(train_total_predictions).reshape(-1)).split('\n')[2:-5]],
-            dtype=np.float16)
-
-        print('Train')
-        for i, train_result in enumerate(train_results):            
-            print(f"[Final] [label {i}] "
-                f"Precision: {train_result[0]:.04f} | "
-                f"Recall: {train_result[1]:.04f} | "
-                f"F1: {train_result[2]:.04f} | ")
-
-        # Validation phase
-        model.eval()
-        valid_loss = 0.0
-        valid_corrects = 0
-        valid_total_samples = 0
-        with torch.no_grad():
-            for batch in tqdm(valid_dataloader, desc='valid iter'):
-                ## data에서 X, y 정의
-                inputs = batch['X']
-                labels = batch['y'].squeeze()
-                inputs, labels = inputs.to(device), labels.to(device)
-                outputs, hiddens = model({'X': inputs, 'y': labels}) # [batch_size, 1]
-
-                loss = criterion(outputs.reshape(-1, config.get('train').get('n_features')), labels.long().flatten())
-                valid_loss += loss.item()
-
-                ## accuracy 추가
-                predictions = outputs.argmax(axis=-1)
-                valid_corrects += torch.sum(predictions == labels)
-                valid_total_samples += inputs.size(0)
-
-                valid_total_predictions.extend(predictions.cpu().tolist())
-                valid_total_labels.extend(labels.tolist())
-
-        avg_valid_loss = valid_loss / len(valid_dataloader)
-        valid_losses.append(avg_valid_loss)
-
-        valid_accuracy = valid_corrects.double() / valid_total_samples
-
-        print(f"[Epoch {epoch + 1}/{num_epochs}] "
-              f"Validation Loss: {avg_valid_loss:.04f} | "
-              f"Validation Accuracy: {valid_accuracy:.04f} | ")
-
-        writer.add_scalar('Loss/valid', avg_valid_loss, epoch)
-        writer.add_scalar('Accuracy/valid', valid_accuracy, epoch)
-
-        print('Validation')
-        valid_results = np.array(
-            [re.sub(' {2,}', ' ', i.strip()).split()[1:-1]
-             for i
-             in classification_report(np.array(valid_total_labels).reshape(-1), np.array(valid_total_predictions).reshape(-1)).split('\n')[2:-5]],
-            dtype=np.float16)
-        for i, valid_result in enumerate(valid_results):            
-            print(f"[Final] [label {i}] "
-                f"Precision: {valid_result[0]:.04f} | "
-                f"Recall: {valid_result[0]:.04f} | "
-                f"F1: {valid_result[2]:.04f} | ")
-
-        # ReduceLROnPlateau를 사용하지 않는 경우 lr 업데이트 & 로깅
-        if isinstance(scheduler, optim.lr_scheduler.ReduceLROnPlateau):
-            scheduler.step(valid_loss / len(valid_dataloader))
-        else:
-            scheduler.step()
-
-        current_lr = getattr(scheduler, 'get_last_lr', 
-                             lambda: [scheduler.optimizer.param_groups[0]['lr']])()[0]
-        writer.add_scalar('lr logging', current_lr, epoch)
-
-        # Save the best model
-        if valid_loss < best_loss:
-            best_loss = valid_loss
-            best_model = copy.deepcopy(model).cpu()
-            scripted_model = scripted_model = torch.jit.trace(best_model, {'X': inputs.cpu()})
-            torch.jit.save(scripted_model, model_save_path)
-            print(f"Best model saved to {model_save_path}")
-
-            patience_count = 0
-
-        else:
-            patience_count += 1
+        total_train_loss += loss.item() * inputs.size(0)
+        preds = outputs.argmax(axis=-1)
+        total_train_corrects += torch.sum(preds == labels)
+        total_train_samples += inputs.size(0)
         
-        # prevent overfitting
-        if patience_count > patience:
-            break
+        train_labels.extend(labels.cpu().numpy())
+        train_preds.extend(preds.cpu().numpy())
 
-    return scripted_model
+    avg_train_loss = total_train_loss / total_train_samples
+    train_accuracy = total_train_corrects.double() / total_train_samples
+
+    # Validation loop
+    total_valid_loss = 0.0
+    total_valid_corrects = 0
+    total_valid_samples = 0
+
+    valid_labels = []
+    valid_preds = []
+
+    model.eval()
+    with torch.no_grad():
+        for batch in tqdm(valid_dataloader, desc='Validation'):
+            inputs = batch['X'].to(device)
+            labels = batch['y'].to(device).squeeze()
+
+            outputs, hidden = model({'X':inputs, 'y':labels})
+            loss = criterion(outputs.reshape(-1, config.get('train').get('n_features')), labels.long().flatten())
+
+            total_valid_loss += loss.item() * inputs.size(0)
+            _, preds = torch.max(outputs, 1)
+            total_valid_corrects += torch.sum(preds == labels)
+            total_valid_samples += inputs.size(0)
+            valid_labels.extend(labels.cpu().numpy())
+            valid_preds.extend(preds.cpu().numpy())
+
+    avg_valid_loss = total_valid_loss / total_valid_samples
+    valid_accuracy = total_valid_corrects.double() / total_valid_samples
+
+    return avg_train_loss, train_accuracy, train_preds, train_labels, avg_valid_loss, valid_accuracy, valid_preds, valid_labels
 
 def main(config):
-    ## parsing은 script에서 수행
 
     tensorboard_path = config.get('general').get('tensorboard').get('path')
 
-    # Tensorboard
     log_dir = make_logdir(tensorboard_path, config.get('general').get('exp_name'))
-    writer = SummaryWriter(log_dir=log_dir)
 
-    ## train data merge
+    model_save_dir = os.path.join(log_dir, 'saved_models') 
+    if not os.path.exists(model_save_dir):
+        os.makedirs(model_save_dir)
+    model_save_path = os.path.join(model_save_dir, 'best_model.pth')
+
     data_path = config.get('general').get('data').get('path')
 
-    merged_train_data = pd.read_parquet(data_path)
-    merged_train_data.event = merged_train_data.groupby(['series_id']).event.shift(1)
-    merged_train_data = merged_train_data.dropna()
-    merged_train_data.reset_index(drop=True, inplace=True)
-    merged_train_data.date = merged_train_data.date.astype(np.int32)
-    preprocessed_data = preprocess(merged_train_data)
-
-    window_size = int(config.get('train').get('window_size'))
-    step = config.get('train').get('step')
-
-    train_list = to_list(preprocessed_data, window_size, config, step)
-    train_keys = extract_keys(preprocessed_data, window_size, step)
-
-    for i, train_key in enumerate(train_keys):
-        train_key['X'] = train_list[i][:, 1:]
-        train_key['event'] = train_list[i][0]
-    train_list = train_keys
-
-    valid_set_size = config.get('train').get('valid_size', 0.2)
-
-    train_data_list, valid_data_list = train_test_split(train_list,
-                                                        test_size=valid_set_size,
-                                                        shuffle=True,
-                                                        random_state=config.get('train').get('random_seed'))
-
-    train_dataset = ChildInstituteDataset(train_data_list)
-    valid_dataset = ChildInstituteDataset(valid_data_list)
-
-    batch_size = config.get('train').get('batch_size')
-    train_data_shuffle = config.get('train').get('data').get('shuffle')
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=train_data_shuffle)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
-
-
-    example_batch = next(iter(train_dataloader))
-    
-    _, n_features, seq_len = example_batch['X'].shape
-    config['train'].update({'seq_len': seq_len, 'n_features': n_features})
-
-    ### train ###
     model_name = config.get('train').get('model')
     model_class = getattr(models, model_name)
-
     model = model_class(config)
 
-    train(
-        config=config,
-        model=model,
-        train_dataloader=train_dataloader,
-        valid_dataloader=valid_dataloader,
-        writer=writer,
-    )
+    series_ids = pd.read_parquet(data_path, columns=['series_id']).series_id.unique()
+    
+    for epoch in tqdm(range(config.get('train').get('epochs'))):
+        total_train_loss = 0.0
+        total_valid_loss = 0.0
+        total_train_corrects = 0
+        total_valid_corrects = 0
+        total_train_samples = 0
+        total_valid_samples = 0
+        all_train_preds = []
+        all_train_labels = []
+        all_valid_preds = []
+        all_valid_labels = []
 
-    writer.close()
+        for series_id in series_ids:
+            series_data = pd.read_parquet(data_path, filters=[('series_id', '=', series_id)])
+            series_data.event = series_data.groupby(['series_id']).event.shift(1)
+            series_data.dropna(inplace=True)
+            series_data.reset_index(drop=True, inplace=True)
+            series_data.date = series_data.date.astype(np.int32)
+            
+            ##Label mapping
+            label_mapping = {0: 0, 1: 1, 3: 2, 4: 3}
+            series_data['event'] = [label_mapping[label] for label in series_data['event']]
+            
+            preprocessed_data = preprocess(series_data)
 
-    with open(f'./{log_dir}/configs.pickle', 'wb') as file:
+            window_size = int(config.get('train').get('window_size'))
+            step = config.get('train').get('step')
+
+            train_list = to_list(preprocessed_data, window_size, config, step)
+            train_keys = extract_keys(preprocessed_data, window_size, step)
+
+            for i, train_key in enumerate(train_keys):
+                train_key['X'] = train_list[i][:, 1:]
+                train_key['event'] = train_list[i][0]
+            train_list = train_keys
+
+            valid_set_size = config.get('train').get('valid_size', 0.2)
+            train_data_list, valid_data_list = train_test_split(train_list, test_size=valid_set_size, shuffle=True, random_state=config.get('train').get('random_seed'))
+
+            train_dataset = ChildInstituteDataset(train_data_list)
+            valid_dataset = ChildInstituteDataset(valid_data_list)
+            
+            batch_size = config.get('train').get('batch_size')
+            train_data_shuffle = config.get('train').get('data').get('shuffle')
+            train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=train_data_shuffle)
+            valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+            
+            train_loss, train_acc, train_preds, train_labels, valid_loss, valid_acc, valid_preds, valid_labels = train(
+                config,
+                model,
+                train_dataloader,
+                valid_dataloader
+            )
+
+            total_train_loss += train_loss * len(train_dataloader.dataset)
+            total_train_corrects += train_acc * len(train_dataloader.dataset)
+            total_train_samples += len(train_dataloader.dataset)
+            all_train_preds.extend(train_preds)
+            all_train_labels.extend(train_labels)
+
+            total_valid_loss += valid_loss * len(valid_dataloader.dataset)
+            total_valid_corrects += valid_acc * len(valid_dataloader.dataset)
+            total_valid_samples += len(valid_dataloader.dataset)
+            all_valid_preds.extend(valid_preds)
+            all_valid_labels.extend(valid_labels)
+
+        avg_train_loss = total_train_loss / total_train_samples
+        avg_valid_loss = total_valid_loss / total_valid_samples
+        train_accuracy = total_train_corrects / total_train_samples
+        valid_accuracy = total_valid_corrects / total_valid_samples
+
+        print(f'Epoch {epoch+1}/{config.get("train").get("epochs")}:')
+        print(f'Train Loss: {avg_train_loss:.4f}, Accuracy: {train_accuracy:.4f}')
+        print(classification_report(train_labels, train_preds, target_names=['Class 0', 'Class 1', 'Class 3', 'Class 4']))
+        print(f'Valid Loss: {avg_valid_loss:.4f}, Accuracy: {valid_accuracy:.4f}')
+        print(classification_report(valid_labels, valid_preds, target_names=['Class 0', 'Class 1', 'Class 3', 'Class 4']))
+
+
+        if avg_valid_loss < best_loss:
+            best_loss = avg_valid_loss
+            best_model = copy.deepcopy(model)
+            torch.jit.save(torch.jit.script(best_model), model_save_path)
+
+    with open(f'./configs.pickle', 'wb') as file:
         pickle.dump(config, file)
